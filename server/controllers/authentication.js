5c278af1dec976b1cc141623d32a5c5872be2ff1
Andy Eccleston <eandy5000@yahoo.com>
2017-01-14 09:07:06 -0600

    good reabuild spot 1

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,12 +3,31 @@ const User = require('../models/user')
 exports.signup = function(req, res, next) {
     const email = req.body.email
     const password = req.body.password
-    console.log(email, password)
+    
+    if(!email || !password) {
+        res.json({error: 'must provide email and password'})
+    }
+
     //see if email users exists
+    User.findOne({email: email}, function(err, existingUser){
+        if(err) {return next(err)}
+        //if it does exist return an error
+        if(existingUser) {
+            return res.status(422)
+                      .send({error: "user already exists"})
+        }
+        //if it is a new email create a new users
+        const user = new User({
+            email, password
+        })
+        user.save(function(err){
+            if(err) {return next(err)}
+            res.json({success: "user created"})
+        })
+       
+             
+    })
 
-    //if it does exist return an error
-
-    //if it is a new email create a new users
 
     //respond to the request
 }
