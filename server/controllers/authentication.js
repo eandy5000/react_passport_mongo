958e55f06c62d2fd373572675e0effd99d7d239a
Andy Eccleston <eandy5000@yahoo.com>
2017-01-15 11:47:59 -0600

    boilerplate api with passport auth working

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -7,6 +7,11 @@ function tokenForUser(user){
     return jwt.encode({sub: user.id, iat: timestamp}, config.secret)
 }
 
+exports.signin = function(req, res, next) {
+//they have already been authed now they need to be issued a token
+    res.send({token: tokenForUser(req.user)})
+}
+
 exports.signup = function(req, res, next) {
     const email = req.body.email
     const password = req.body.password
--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,25 @@ const User = require('../models/user')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const localStrategy = require('passport-local')
+
+//local strategy
+const localOptions = { usernameField: 'email' }
+const localLogin = new localStrategy(localOptions, function(email, password, done){
+    //verify email and password call done with user
+    //else call done with false
+    User.findOne({email: email}, function(err, user){
+        if(err) {return done(err)}
+        if(!user) {return done(null, false)}
+       // compare password is 'password' equal to user.password
+       user.comparePassword(password, function(err, isMatch){
+           if(err) {return done(err)}
+           if(!isMatch) {return done(null, false)}
+
+           return done(null, user)
+       })
+    })
+})
 
 //options for jwt Strategy
 const jwtOptions = {
@@ -30,6 +49,7 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
 
 //tell passport to use this Strategy
 passport.use(jwtLogin)
+passport.use(localLogin)
 
 
 
