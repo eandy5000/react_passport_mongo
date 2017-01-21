d5b66e5bd5892f8dc83061cd7a8767d1d7b064dd
Andy Eccleston <eandy5000@yahoo.com>
2017-01-21 14:41:33 -0600

    express basics up and running

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const http = require('http')
+const router = require('./router')
+
+//app config
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json({type: '*/*' }))
+router(app)
+
+//server setup
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
+server.listen(port)
+console.log("listening on port: ", port)
 
 
 
