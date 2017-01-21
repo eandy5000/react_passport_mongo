b104603afa66be32b8fb2e6723f55d3c00ea794e
Andy Eccleston <eandy5000@yahoo.com>
2017-01-21 14:40:56 -0600

    reset app for rebuild

--- server/index.js
+++ server/index.js
@@ -1,24 +1,3 @@
-const express = require('express')
-const http = require('http')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const router = require('./router')
-const mongoose = require('mongoose')
 
-//db connection
-mongoose.connect('mongodb://localhost:auth/auth_users')
-
-//app setup
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json({type: '*/*'}))
-router(app)
-
-//serversetup
-const port = process.env.PORT || 3090
-const server = http.createServer(app)
-
-server.listen(port)
-console.log('listening on port: ',port)
 
 
